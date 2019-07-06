f35211cec3be2cdd431abef004cef76106ce3be6
M7cool <4314335+zyzski@users.noreply.github.com>
2019-07-06 18:56:19 -0400

    explain script commands

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 import Layout from '../src/components/Layout/Layout';
@@ -5,6 +6,10 @@ import StyledLink from '../src/components/StyledLink/StyledLink';
 
 const Index = () => (
   <Layout>
+    <Head>
+      <title>zYzSkI's nextjs starter</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1>Welcome to the index page</h1>
     <Link href="/post/1">
       <StyledLink>View Post</StyledLink>
